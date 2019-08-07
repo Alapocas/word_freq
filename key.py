8504774b9c1655fe3f2a5e6d4f07257418c30187
Alapocas <sissijian0720@163.com>
2019-08-07 15:23:22 +0800

    SQL added

--- key.py
+++ key.py
@@ -5,4 +5,4 @@ MAX_NUM = 10000 #最多条数
 MAX_FREQ = 50 #词频排行上限
 PATH = ""#字典的绝对路径
 PATH_B = ""#黑名单文件的绝对路径
-SQL = []
+SQL = ["SELECT DISTINCT enterprise_id FROM tb_enterprise_entity", "SELECT ba.currency FROM c4c_bank_account ba INNER JOIN tb_enterprise_entity tee ON tee.le_id = ba.le_id AND tee.enterprise_id = %s GROUP BY ba.currency", "SELECT COUNT(*) FROM c4c_bank_statement a INNER JOIN tb_enterprise_entity b on a.LE_ID = b.le_id WHERE enterprise_id = %s and CURRENCY = %s", "SELECT CUSTOMER_ACCOUNT_NAME, USER_MEMO FROM c4c_bank_statement a INNER JOIN tb_enterprise_entity b on a.LE_ID = b.le_id WHERE enterprise_id = %s and CURRENCY = %s LIMIT %s, %s"]
