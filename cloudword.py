610d541e685c54f5419fd47b737da7d9d8691616
Alapocas <sissijian0720@163.com>
2019-08-07 15:03:06 +0800

    some updates

--- cloudword.py
+++ cloudword.py
@@ -1,52 +1,79 @@
 #coding:utf-8
-import pymysql, json, time, re, pickle, os, collections, math, requests
+import pymysql, json, time, re, pickle, os, collections, math, requests, os
 import jieba_fast as jieba
 #import jieba #用windows跑的时候请在读文件的时候加上encoding参数并修改jieba库的使用
 jieba.initialize()
-from key import DBS, MAX_NUM, MAX_FREQ, PATH, SQL
+from key import DBS, MAX_NUM, MAX_FREQ, PATH, PATH_B, SQL
+black_list = []
+if os.path.exists(PATH_B):
+    black_list = open(PATH_B, "r").read()
+    black_list = black_list.split("\n")
+else:
+    black_list = [""]
 filtrate = re.compile(u'[^\u4e00-\u9fa5A-Za-z]+')
-final = {}
+final_result = {}
 for name in DBS.keys():
     conn = pymysql.connect(DBS[name]["DBPATH"], DBS[name]["USER"], DBS[name]["PWD"], DBS[name]["DB"],charset='utf8')
     curs = conn.cursor()
     curs.execute(SQL[0])
-    result = curs.fetchall()
-    result = [i[0] for i in result]
-    total = {}
+    companies = curs.fetchall()
+    companies = [i[0] for i in companies]
+    currencies = {}
     t1=time.time()
     jieba.enable_parallel() #线程不是开得越多越好，得按需求来
-    for i in result[:30]:
+    for i in companies:
         curs.execute(SQL[1], [i])
         currency = curs.fetchall()
         if len(currency)>0:
             currency = [i[0] for i in currency]
         for j in currency:
             curs.execute(SQL[2], [i, j])
-            info_num = curs.fetchall()[0][0]
-            loop = math.ceil(info_num/MAX_NUM)
+            num_of_rows = curs.fetchall()[0][0]
+            loop = math.ceil(num_of_rows/MAX_NUM)
             if loop == 0:
                 break
             uid = str(i)+','+j
-            total[uid] = {"name":{}, "memo":{}}
+            currencies[uid] = {"name": {}, "memo": {}}
             for k in range(loop):
                 curs.execute(SQL[3], [i, j, k*MAX_NUM, MAX_NUM])
-                res = curs.fetchall()
-                tmp = {}
-                tmp["name"] = dict(collections.Counter([word for word in jieba.lcut(" ".join([raw[0] for raw in res if raw != "NONE"])) if re.search(filtrate, word) == None]).most_common(MAX_FREQ))
-                tmp["memo"] = dict(collections.Counter([word for word in jieba.lcut(" ".join([raw[1] for raw in res if raw != None])) if re.search(filtrate, word) == None]).most_common(MAX_FREQ))
-                for key, value in tmp["name"].items():
-                    if key in total[uid]["name"]:
-                        total[uid]["name"][key] += value
+                raw_content = curs.fetchall()
+                currency = {"name": {}, "memo": {}}
+                processed = ["", ""]
+                for raw in raw_content:
+                    if raw[0] != "NONE" and raw[0] != None:
+                        processed[0] += raw[0] + " "
+                    if raw[1] != "NONE" and raw[1] != None:
+                        processed[1] += raw[1] + " "
+                processed = [jieba.lcut(proc.strip()) for proc in processed]
+                processed = [list(filter(lambda x: re.search(filtrate, x) == None, proc)) for proc in processed]
+                words = collections.Counter(processed[0]).most_common()
+                position = 0
+                while True:
+                    if position == len(words) or len(currency["name"]) == 50:
+                        break
+                    elif words[position][0] not in black_list:
+                        currency["name"].update([words[position]])
+                    position += 1
+                words = collections.Counter(processed[1]).most_common()
+                position = 0
+                while True:
+                    if position == len(words) or len(currency["memo"]) == 50:
+                        break
+                    elif words[position][0] not in black_list:
+                        currency["memo"].update([words[position]])
+                    position += 1
+                for key, value in currency["name"].items():
+                    if key in currencies[uid]["name"]:
+                        currencies[uid]["name"][key] += value
                     else:
-                        total[uid]["name"][key] = value
-                for key, value in tmp["memo"].items():
-                    if key in total[uid]["memo"]:
-                        total[uid]["memo"][key] += value
+                        currencies[uid]["name"][key] = value
+                for key, value in currency["memo"].items():
+                    if key in currencies[uid]["memo"]:
+                        currencies[uid]["memo"][key] += value
                     else:
-                        total[uid]["memo"][key] = value
-    final[name] = total
-    print(final)
-pickle.dump(final, open(PATH, "wb"), protocol=2)
+                        currencies[uid]["memo"][key] = value
+    final_result[name] = currencies
+pickle.dump(final_result, open(PATH, "wb"), protocol=2)
 requests.get("http://127.0.0.1:5000/reload")
 t2=time.time()
 with open("/tmp/test.txt", "a") as ff:
